b98deb6b55683dc7daaa450871a6b15f45ca2e03
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-06-05 23:21:59 +0530

    chapter2.txt

---
+++ chapter2.cpp
@@ -0,0 +1,29 @@
+Learning self discipline:-
+self discipline cant happen in one or two days it will take time coz during this period u have to work on ur will.
+self_discipline is correction and regulation of one self in order to improve.it is the ability of u to control urself and stay 
+on track what is right.
+
+how does self-discipline help?
+1) self discipline happens when ur brain believe that ur goal is achievable and worth while.
+2) self discipline concerns sensible goals.
+3) your decision is made before u can become consious of them.
+4) UR HAbitual behaiour are assembled for the entire life time.
+5) ur estimate of how long will it take to do something decides ur energy level.
+6) u will work only when u think that a particular goal is achievable.
+7) clearly defining ur goals bost ur self decipline.// person who is coming in mind after hearing this statement is me.
+8) it helps u plan and set ur daily routines.
+
+self discipline is about convincing the mind.
+goal should be really big in ur life and ur brain should think that it is achievable.
+
+specifying clear goals:
+we need to tell our brain clear actions and also to have a proper schedule to follow.
+along with this we need to make a clear note of all the actions in order to achieve it.
+
+visualisation is very helpful in keeping one's to self discipline if we can predict or see where we want to stand and make that 
+picture our brain will work really hard to make that picture a reality.
+change routines.
+
+lastly there is a very famous quote 'we must all suffer one of the two things: pain of discipline or pain of regret and
+disappointment'.
+
